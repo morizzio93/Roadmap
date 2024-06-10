3f3ce75c93ee37e50a75cf20d2f9d70f049e4f90
Mzo <morizzio93+git@gmail.com>
2024-06-10 16:41:06 +0200

    Implement routing system with React Router and update project structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import Form from './Form';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-      <Form />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Feature</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
